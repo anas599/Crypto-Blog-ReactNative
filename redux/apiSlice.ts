d099a9030db54d1110e16fb624f1ab0beb3c3715
anas599 <anas599@yahoo.com>
2023-11-12 12:54:08 +0300

    add slice and store for redux

---
+++ redux/apiSlice.ts
@@ -0,0 +1,44 @@
+
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  data: [],
+  loading: "idle",
+  error: null,
+};
+
+export const fetchData = createAsyncThunk("api/fetchData", async () => {
+    const response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=5&convert=USD', {
+        headers: {
+            "X-CMC_PRO_API_KEY": "7974008e-e711-40f4-8b21-42c19b00e602",
+        },
+    });
+
+    return response.data;
+});
+
+const apiSlice = createSlice({
+  name: "api",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchData.pending, (state) => {
+        state.loading = "loading";
+      })
+      .addCase(fetchData.fulfilled, (state, action) => {
+        state.loading = "idle";
+        state.data = action.payload;
+      })
+      .addCase(fetchData.rejected, (state, action) => {
+        state.loading = "idle";
+        // @ts-ignore
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const selectData = (state: { api: { data: any; }; }) => state.api.data;
+
+export default apiSlice.reducer;
