44d2bec718d7fe4f23737db6dcdce2c601c3fe95
anas599 <anas599@yahoo.com>
2023-11-13 12:44:35 +0300

    add comments slice for redux

---
+++ redux/commentsSlice.ts
@@ -0,0 +1,41 @@
+
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  data: [],
+  loading: "idle",
+  error: null,
+};
+
+export const fetchComments = createAsyncThunk("api/fetchComments", async () => {
+    const response = await axios.get('https://crypto-blog-nextjs.netlify.app/api/comments', {
+        
+    });
+
+    return response.data;
+});
+
+const commentsSlice = createSlice({
+  name: "comments",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchComments.pending, (state) => {
+        state.loading = "loading";
+      })
+      .addCase(fetchComments.fulfilled, (state, action) => {
+        state.loading = "idle";
+        state.data = action.payload;
+      })
+      .addCase(fetchComments.rejected, (state, action) => {
+        state.loading = "idle";
+        // @ts-ignore
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const selectComments = (state: { comments: { data: any; }; }) => state.comments.data;
+export default commentsSlice.reducer;
