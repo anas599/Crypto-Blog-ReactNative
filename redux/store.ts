e2ac47f9af41055fc04b1db002534a0bdd5033d3
anas599 <anas599@yahoo.com>
2023-11-13 12:42:44 +0300

    add comment reducer to store

--- redux/store.ts
+++ redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import apiReducer from "./apiSlice";
+import commentsReducer from "./commentsSlice";
 
 const store = configureStore({
   reducer: {
     api: apiReducer,
+    comments: commentsReducer
   },
 });
 
