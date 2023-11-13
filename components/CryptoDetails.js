da23983cb927714c814aadaf25bebea8b04cc48f
anas599 <anas599@yahoo.com>
2023-11-13 12:43:38 +0300

    fetch comments and filter it in crypto details

--- components/CryptoDetails.js
+++ components/CryptoDetails.js
@@ -2,12 +2,19 @@ import React, { useEffect, useState } from "react";
 import { Text, View, StyleSheets } from "../components/Themed";
 import { styles } from "../screens/TabOneScreen";
 import { useDispatch, useSelector } from "react-redux";
-
+import { fetchComments, selectComments } from "../redux/commentsSlice";
+import { FEATURE_ENABLED } from "@env";
 import { fetchData, selectData } from "../redux/apiSlice";
 
 function CryptoDetails({ route }) {
   const [crypto, setCrypto] = useState(null);
   const dataRedux = useSelector(selectData);
+  const commentsRedux = useSelector(selectComments);
+  const nameLower = crypto?.name?.toLowerCase();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchComments());
+  }, [dispatch]);
 
   useEffect(() => {
     const selectedCrypto = dataRedux.data.find(
@@ -15,19 +22,39 @@ function CryptoDetails({ route }) {
     );
     setCrypto(selectedCrypto);
   }, [route.params.id, dataRedux.data]);
+
+  const mycomment = commentsRedux;
+  const filterComment =
+    mycomment?.length === undefined
+      ? []
+      : mycomment?.filter((item) => item.coincommentId === nameLower);
+  // console.log(filterComment[0].username);
+  let author;
+  if (filterComment && filterComment.length > 0) {
+    author = filterComment[0].username;
+  }
+
   return (
     <>
       <View style={styles.container}>
         {crypto && (
           <>
-            <Text>cr{crypto.name}</Text>
+            <Text>{crypto.name}</Text>
             <Text>{crypto.quote.USD.price}</Text>
-            <Text>this is a test</Text>
+            {filterComment.length > 0
+              ? filterComment.map((item) => (
+                  <Text key={item.id} style={styles.textBorder}>
+                    <Text style={[styles.textBorder, styles.userName]}>
+                      {author}
+                    </Text>
+                    {item.content}
+                  </Text>
+                ))
+              : null}
           </>
         )}
       </View>
     </>
   );
 }
-
 export default CryptoDetails;
