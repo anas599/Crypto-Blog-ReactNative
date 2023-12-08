fabd3d12c2416ec49c9ab0be3ad51c35ae6868b5
anas599 <anas599@yahoo.com>
2023-12-08 18:03:35 -0500

    Add environment variables declaration

---
+++ env.d.ts
@@ -0,0 +1,11 @@
+declare module '@env' {
+  export const FIREBASE_API_KEY: string;
+  export const PRO_API_KEY : string;
+  export const COMMENTS_API : string;
+  export const  AUTH_DOMAIN : string;
+    export const  PROJECT_ID : string;
+    export const  STORAGE_BUCKET : string;
+    export const  MESSAGING_SENDER_ID : string;
+    export const APP_ID : string;
+
+}
